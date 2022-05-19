669955a61ed18f843f40707d515a52d387cc902e
Miguel00n11 <90936808+Miguel00n11@users.noreply.github.com>
2022-05-19 01:39:16 -0500

    Caratula de empiezo1

---
+++ app/src/main/java/com/example/rocaapp/HomeActivity.kt
@@ -0,0 +1,10 @@
+package com.example.rocaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
+class HomeActivity:AppCompatActivity(){
+    private val db= FirebaseFirestore.getInstance()
+
+
+}
--- app/src/main/java/com/example/rocaapp/MainActivity.kt
+++ app/src/main/java/com/example/rocaapp/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.rocaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val guadar=findViewById<Button>(R.id.bGuardar)
+//        val recuperar=findViewById<Button>(R.id.bRecuperar)
+//        val eliminar=findViewById<Button>(R.id.bEliminar)
+//
+//        val direccion=findViewById<EditText>(R.id.tvDireccion)
+//        val email=findViewById<EditText>(R.id.tvEmail)
+//        val phone=findViewById<EditText>(R.id.tvPhone)
+//
+
+
+//        guadar.setOnClickListener(){
+//
+//            db.collection("users").document(email.text.toString()).
+//            set(hashMapOf("provider" to direccion.text.toString(),"phone" to phone.text.toString()))
+//        }
+//        recuperar.setOnClickListener(){
+//
+//        }
+//        eliminar.setOnClickListener(){
+//
+//        }
+
     }
+
 }
