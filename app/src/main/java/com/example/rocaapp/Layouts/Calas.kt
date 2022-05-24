ca3ee016575bd0f4e647982cd7fda5a5fbc52c37
Miguel00n11 <90936808+Miguel00n11@users.noreply.github.com>
2022-05-23 23:12:01 -0500

    dsef233

--- app/src/main/java/com/example/rocaapp/Layouts/Calas.kt
+++ app/src/main/java/com/example/rocaapp/Layouts/Calas.kt
@@ -2,6 +2,10 @@ package com.example.rocaapp.Layouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.rocaapp.R
 
 class Calas : AppCompatActivity() {
@@ -9,6 +13,31 @@ class Calas : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calas)
 
+        val peso=findViewById<EditText>(R.id.etPesomaterialCala)
+        val volumen=findViewById<EditText>(R.id.etVolumenCala)
+        val humedad=findViewById<EditText>(R.id.etHuemdadCala)
 
+        val resultado=findViewById<TextView>(R.id.tvResultadoCala)
+
+        val calcular=findViewById<Button>(R.id.bCalcularCala)
+        val regresar=findViewById<Button>(R.id.bRegresarCalas)
+
+        calcular.setOnClickListener(){
+            resultado.text= CalcularCala(
+                peso=peso.text.toString().toDouble(),
+                humedad = humedad.text.toString().toDouble(),
+                volumen = volumen.text.toString().toDouble()).toString()
+        }
+
+        regresar.setOnClickListener(){onBackPressed()}
+
+
+
+
+    }
+    private fun CalcularCala( peso:Double, humedad:Double, volumen: Double): Double {
+
+        val AA=peso * volumen * humedad
+        return AA
     }
 }
