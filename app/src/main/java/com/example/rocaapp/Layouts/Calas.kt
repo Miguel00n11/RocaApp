b29d37713988b44ef788f67949bacb5ce55967af
Miguel Rodriguez <rooj_civ@hotmail.com>
2022-06-17 13:15:33 -0500

    qwedqwe

--- app/src/main/java/com/example/rocaapp/Layouts/Calas.kt
+++ app/src/main/java/com/example/rocaapp/Layouts/Calas.kt
@@ -14,24 +14,26 @@ class Calas : AppCompatActivity() {
         setContentView(R.layout.activity_calas)
 
         val mvsm=findViewById<EditText>(R.id.etMVSM)
+        val mvarena=findViewById<EditText>(R.id.etMVarena)
         val peso=findViewById<EditText>(R.id.etPesomaterialCala)
-        val volumen=findViewById<EditText>(R.id.etVolumenCala)
+        val Warena=findViewById<EditText>(R.id.etWarena)
         val humedad=findViewById<EditText>(R.id.etHuemdadCala)
 
         val resultado=findViewById<TextView>(R.id.tvResultadoCala)
         val resultadoCompactacion=findViewById<TextView>(R.id.tvResultadoCompactacion)
 
         val calcular=findViewById<Button>(R.id.bCalcularCala)
-        val regresar=findViewById<Button>(R.id.bRegresarCalas)
+//        val regresar=findViewById<Button>(R.id.bRegresarCalas)
 
         calcular.setOnClickListener(){
             
             try {
-                resultado.text= CalcularCala(
+                resultado.text= "%.2f".format( CalcularCala(
                     peso=peso.text.toString().toDouble(),
                     humedad = humedad.text.toString().toDouble(),
-                    volumen = volumen.text.toString().toDouble()).toString()
-                resultadoCompactacion.text=(resultado.text.toString().toDouble()/mvsm.text.toString().toDouble()*100).toString()
+                    Warena = Warena.text.toString().toDouble(),
+                    MVarena = mvarena.text.toString().toDouble())).toString()
+                resultadoCompactacion.text= (("%.2f".format( resultado.text.toString().toDouble()/mvsm.text.toString().toDouble()*100)) + " %").toString()
 
             }catch (e:Exception){
                 Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
@@ -40,15 +42,18 @@ class Calas : AppCompatActivity() {
 
         }
         
-        regresar.setOnClickListener(){onBackPressed()}
+//        regresar.setOnClickListener(){onBackPressed()}
 
 
 
 
     }
-    private fun CalcularCala( peso:Double, humedad:Double, volumen: Double): Double {
+    private fun CalcularCala( peso:Double, humedad:Double, Warena: Double,MVarena:Double): Double {
 
-        val mvsMuestra=peso/volumen/(1+humedad/100)
+        val VolArena=Warena/MVarena
+        val mvsMuestra=peso/VolArena/(1+humedad/100)
+
+//        val mvsMuestra=mvMuestra(1+humedad/100)
         return mvsMuestra
     }
 }
